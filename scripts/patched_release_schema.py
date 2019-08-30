500a9cd3e73bfe4846b3b8b9da2435246c8658ed
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-08-30 09:36:13 -0400

    Add scripts for mapping sheet for all extensions

---
+++ scripts/patched_release_schema.py
@@ -0,0 +1,36 @@
+"""
+python scripts/patched_release_schema.py > release-schema-patched.json
+ocdskit mapping-sheet --infer-required --extension-field extension release-schema-patched.json > mapping-sheet.csv
+"""
+
+import json
+import sys
+
+from ocdsextensionregistry import ExtensionRegistry, ProfileBuilder
+from ocdsextensionregistry.util import get_latest_version
+
+extensions = {}
+
+url = 'https://raw.githubusercontent.com/open-contracting/extension_registry/master/extension_versions.csv'
+for version in ExtensionRegistry(url):
+    if version.id not in extensions:
+        extensions[version.id] = []
+    extensions[version.id].append(version)
+urls = [get_latest_version(versions).download_url for versions in extensions.values()]
+
+# TODO: Uncomment extensions once PRs merged.
+builder = ProfileBuilder('1__1__4', urls + [
+    'https://github.com/open-contracting-extensions/ocds_awardCriteria_extension/archive/master.zip',
+    # 'https://github.com/open-contracting-extensions/ocds_bidOpening_extension/archive/master.zip',
+    'https://github.com/open-contracting-extensions/ocds_contractTerms_extension/archive/master.zip',
+    'https://github.com/open-contracting-extensions/ocds_coveredBy_extension/archive/master.zip',
+    # 'https://github.com/open-contracting-extensions/ocds_eu_extension/archive/master.zip',
+    'https://github.com/open-contracting-extensions/ocds_options_extension/archive/master.zip',
+    'https://github.com/open-contracting-extensions/ocds_procedure_extension/archive/master.zip',
+    'https://github.com/open-contracting-extensions/ocds_secondStageDescription_extension/archive/master.zip',
+    'https://github.com/open-contracting-extensions/ocds_submissionTerms_extension/archive/master.zip',
+    'https://github.com/open-contracting-extensions/ocds_techniques_extension/archive/master.zip',
+])
+
+schema = builder.patched_release_schema(extension_field='extension')
+json.dump(schema, sys.stdout, ensure_ascii=False, indent=2, separators=(',', ': '))
