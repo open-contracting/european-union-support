0a9c9bf8b8b29b0ccc77b0e5fb6dea16f716fdd4
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-04-24 11:59:11 -0400

    Simplify patching script

--- scripts/patched_release_schema.py
+++ scripts/patched_release_schema.py
@@ -1,46 +1,14 @@
 import json
 import sys
 
+import requests
 from ocdsextensionregistry import ExtensionRegistry, ProfileBuilder
 from ocdsextensionregistry.util import get_latest_version
 
-builder = ProfileBuilder('1__1__4', {
-    'additionalContactPoint': 'master',
-    'bids': 'master',
-    'charges': 'master',
-    'finance': 'master',
-    'location': 'master',
-    'lots': 'master',
-    'metrics': 'master',
-    'participation_fee': 'master',
-    'partyScale': 'master',
-    'process_title': 'master',
-    'shareholders': 'master',
-})
+url = 'https://raw.githubusercontent.com/open-contracting-extensions/european-union/master/docs/extension_versions.json'  # noqa: E501
+
+builder = ProfileBuilder('1__1__4', requests.get(url).json())
 
 schema = builder.patched_release_schema(extension_field='extension')
 
-# TODO: Uncomment extensions once PRs merged.
-builder = ProfileBuilder('1__1__4', [
-    'https://github.com/open-contracting-extensions/ocds_awardCriteria_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_bidOpening_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_communication_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_contractTerms_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_coveredBy_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_designContest_extension/archive/master.zip',
-    # 'https://github.com/open-contracting-extensions/ocds_eu_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_options_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_otherRequirements_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_organizationClassification_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_procedure_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_procurementMethodRationaleClassifications_extension/archive/master.zip',  # noqa: E501
-    'https://github.com/open-contracting-extensions/ocds_secondStageDescription_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_selectionCriteria_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_subcontracting_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_submissionTerms_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_techniques_extension/archive/master.zip',
-    'https://github.com/open-contracting-extensions/ocds_unstructuredChanges_extension/archive/master.zip',
-])
-
-schema = builder.patched_release_schema(extension_field='extension', schema=schema)
 json.dump(schema, sys.stdout, ensure_ascii=False, indent=2, separators=(',', ': '))
