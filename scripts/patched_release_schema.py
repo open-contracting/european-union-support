a54da387ce24e46a86ae85fe19157581783e0cce
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-10-29 17:15:03 -0400

    flake8

--- scripts/patched_release_schema.py
+++ scripts/patched_release_schema.py
@@ -2,8 +2,7 @@ import json
 import sys
 
 import requests
-from ocdsextensionregistry import ExtensionRegistry, ProfileBuilder
-from ocdsextensionregistry.util import get_latest_version
+from ocdsextensionregistry import ProfileBuilder
 
 url = 'https://raw.githubusercontent.com/open-contracting-extensions/european-union/master/docs/extension_versions.json'  # noqa: E501
 
