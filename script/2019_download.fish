c375173ee60fedc56ec909fa9cff59a7e8306ad7
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-04-29 00:30:47 -0400

    eforms: Automate download of eForms annex

--- script/2019_download.fish
+++ script/2019_download.fish
@@ -8,3 +8,7 @@ curl -sS -o 'XPATHs provisional release v. 1.0.docx' https://simap.ted.europa.eu
 
 # 2022-04-13 mapping in Excel
 curl -sS -o TED-XML-to-eForms-mapping-OP-public-20220404.xlsx https://simap.ted.europa.eu/documents/10184/320101/TED-XML-to-eForms-mapping-OP-public-20220404/a0fed751-76cb-491b-957d-96985fdc82a4
+
+# https://ec.europa.eu/growth/single-market/public-procurement/digital-procurement/eforms_en
+
+curl -sS -o CELEX_32019R1780_EN_ANNEX_TABLE2_Extended.xlsx https://ec.europa.eu/docsroom/documents/43488/attachments/1/translations/en/renditions/native
