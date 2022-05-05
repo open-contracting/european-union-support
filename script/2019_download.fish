2c098dad298370636d710f6232d522b7c1a7e438
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-05-05 16:04:35 -0400

    eforms: Start transition to field-based mapping instead of form-BT-based

--- script/2019_download.fish
+++ script/2019_download.fish
@@ -3,10 +3,7 @@ cd source
 
 # https://simap.ted.europa.eu/en_GB/web/simap/eforms
 
-# 2020-05-20 version 1.0 of the mapping between eForms components and Business Terms
-curl -sS -o 'XPATHs provisional release v. 1.0.docx' https://simap.ted.europa.eu/documents/10184/320101/XPATHs+provisional+release+v.+1.0/f74a6976-af15-4bad-99ce-9a4684b60dba
-
-# 2022-04-13 mapping in Excel
+# 2022-04-13 "This mapping in Excel" updates "eForms documentation v. 1.1 and XPATHs provisional release v. 1.0 (25/05/2020)".
 curl -sS -o TED-XML-to-eForms-mapping-OP-public-20220404.xlsx https://simap.ted.europa.eu/documents/10184/320101/TED-XML-to-eForms-mapping-OP-public-20220404/a0fed751-76cb-491b-957d-96985fdc82a4
 
 # https://ec.europa.eu/growth/single-market/public-procurement/digital-procurement/eforms_en
