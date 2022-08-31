b01c0ec8cbea6492bd3866d811558aec5ceead38
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-08-31 14:56:09 -0400

    eforms: Exclude fields used only in X01 or X02, for WARN severity

--- manage.py
+++ manage.py
@@ -253,19 +253,20 @@ def update_with_sdk(filename):
         df = pd.DataFrame.from_dict(json.load(f)["fields"])
 
     all_notice_types = {str(i) for i in range(1, 41)} | {"CEI", "T01", "T02", "X01", "X02"}
-    for notice_types in (["X01", "X02"], ["X02"]):
-        condition = df["mandatory"] == {
-            "value": False,
-            "severity": "ERROR",
-            "constraints": [{"noticeTypes": notice_types, "value": True, "severity": "ERROR"}],
-        }
-        # Check that the removed rows are forbidden on all except the notice types X01 and X02.
-        for forbidden in df[condition]["forbidden"]:
-            actual = set(forbidden["constraints"][0]["noticeTypes"])
-            expected = all_notice_types - set(notice_types)
-            assert actual == expected, f"symmetric difference: {actual ^ expected}"
-        print(df[condition]["id"])
-        df = df[~condition]
+    for severity in ("ERROR", "WARN"):
+        for notice_types in (["X01", "X02"], ["X02"]):
+            condition = df["mandatory"] == {
+                "value": False,
+                "severity": "ERROR",
+                "constraints": [{"noticeTypes": notice_types, "value": True, "severity": severity}],
+            }
+            # Check that the removed rows are forbidden on all except the notice types X01 and X02.
+            for forbidden in df[condition]["forbidden"]:
+                actual = set(forbidden["constraints"][0]["noticeTypes"])
+                expected = all_notice_types - set(notice_types)
+                assert actual == expected, f"symmetric difference: {actual ^ expected}"
+            click.echo(df[condition]["id"].to_string(index=False))
+            df = df[~condition]
 
     # Remove or abbreviate columns that do not assist the mapping process and that lengthen the JSON file. See README.
     drop = ["xpathRelative", "legalType", "maxLength", "forbidden", "assert", "inChangeNotice", "privacy"]
